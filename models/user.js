c9bef4279858b6a41f99936fd910f5a1d31d6ab4
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-04-11 16:30:19 -0700

    add bcrypt and some methods to user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,19 @@
 'use strict';
+const bcrypt = require('bcrypt');
+const Promise = require('bluebird')
+
+var hashPassword = function(password) {
+  return new Promise(function(resolve, reject) {
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) return reject(err);
+      bcrypt.hash(password, salt, function(err, hash) {
+        if (err) return reject(err);
+        resolve(hash);
+      });
+    });
+  });
+}
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     first_name: DataTypes.STRING,
@@ -14,9 +29,28 @@ module.exports = function(sequelize, DataTypes) {
     },
   }, {
     classMethods: {
+      createWithPassword: function(password, attributes) {
+        return hashPassword(password).then(function(hash) {
+          attributes.password = hash;
+          return User.create(attributes);
+        });
+      },
       associate: function(models) {
         // associations can be defined here
       }
+    },
+    instanceMethods: {
+      comparePassword: function(password) {
+        var user = this;
+        return new Promise(function(resolve, reject) {
+          if (!password) return reject();
+          bcrypt.compare(password, user.password || '', function (err, isValid) {
+            if (err) throw err;
+            if (isValid) resolve(user);
+            else reject();
+          });
+        });
+      },
     }
   });
   return User;
