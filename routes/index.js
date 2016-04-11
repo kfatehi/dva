449d295ac34a54876f3343577e737230334c0c2b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-04-11 14:37:13 -0700

    serving users (persistence layer working)

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,8 @@ var router  = express.Router();
 
 router.get('/users', function(req, res) {
   models.User.findAll({
-    include: [ models.Task ]
   }).then(function(users) {
-    res.render('index', {
+    res.send({
       title: 'Express',
       users: users
     });
