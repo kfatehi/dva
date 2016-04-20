d0382116a3996bab174df3b5e812ec4af8d9a97e
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-04-20 15:34:56 -0700

    add react example

---
+++ src/components/TableComponent.js
@@ -0,0 +1,41 @@
+import React from 'React';
+
+class TableComponent extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render () {
+    return (<table>
+      <thead>
+        <tr onClick={this.props.onTitleClick}>
+          <th style={ {color: this.props.title.color} }>
+            {this.props.title.text}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {this.props.rows.map(function (text, idx) {
+          return (<tr key="{idx}">
+              <td>{text}</td>
+          </tr>);
+        })}
+      </tbody>
+    </table>);
+  }
+}
+
+TableComponent.PropTypes = {
+  title: React.PropTypes.shape({
+    text: React.PropTypes.string.isRequired,
+    color: React.PropTypes.string.isRequired
+  }),
+  rows: React.PropTypes.arrayOf(
+    React.PropTypes.string
+  ),
+  onTitleClick: React.PropTypes.function
+};
+
+export default TableComponent;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("watup");
+import * as TableActions from './actions/TableActions';
+import TableReducer from './reducers/TableReducer';
+import TableComponent from './components/TableComponent';
+
+import React from 'react';
+
+import { render } from 'react-dom';
+import { createStore } from 'Redux';
+
+function createTableComponent( container ) {
+
+  let store = createStore(TableReducer);
+
+  function renderComponent() {
+    var state = store.getState();
+    console.log(state);
+    render( <TableComponent
+      title = {state.title}
+      rows = {state.rows}
+      onTitleClick = { () => {
+          store.dispatch(TableActions.changeTitleColor());
+        }
+      }
+    />, container );
+  }
+
+  store.subscribe(renderComponent);
+  renderComponent();
+}
+
+/*title = {state.title}
+rows = {state.rows}
+onTitleClick = { () => {
+    store.dispatch(TableActions.changeTitleColor());
+  }
+}*/
+
+export default createTableComponent;
