91b24f06bd55ba9cb6890a8cdbd89daeb75146cb
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-04-20 14:39:01 -0700

    cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,19 @@
-var path = require('path');
-
 module.exports = {
   devtool: 'eval-source-map',
-  entry: path.join(__dirname, './src/index.js'),
+  entry: `${__dirname}/src/index.js`,
   output: {
-    path: path.join(__dirname, 'public'),
-    library: 'dva',
-    filename: 'js/main.js'
+    path: `${__dirname}/public`,
+    library: 'app',
+    filename: 'js/app.js'
   },
-
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react']
-        }
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react']
       }
-    ]
-  },
-
-  externals: {
-    'React': 'React',
-    'Redux': 'Redux',
-    'react-dom': 'ReactDOM'
+    }]
   }
 };
