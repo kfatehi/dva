d8f79b2f24528b51bf4705362d3339dbd2cafe40
Brett Walker <bdwalker93@yahoo.com>
2016-05-15 16:53:02 -0700

    half way through writing the dataprep

--- src/extensions/com.brett.sankeyV2/sankey-data-prep.js
+++ src/extensions/com.brett.sankeyV2/sankey-data-prep.js
@@ -1,8 +1,33 @@
-export default function (){
+export default function (data){
+  //creates the structure of our prepared data
+  var prepedGraph = {"nodes" : [], "links" : []};
 
-  return { "nodes": [{"names": "bad names"}]}
+  //cycles through the unprepared data to populate our new formatting
+  data.forEach(function(d){
+    //pushes the soure node of a data entry into our node list
+    prepedGraph.nodes.push({"Name" : d['source']});
 
+    //pushes the target node of a data entry into our node list
+    prepedGraph.nodes.push({"Name" : d['target']});
 
+    //populates all of the links
+    prepedGraph.links.push({"source" : d['source'],
+                            "target" : d['target'],
+                            "value"  : parseInt(d['value'])
+                            });
+  });
+  
+  var uniqueNodes = {};
+
+  //we need to rid all of the duplicates from the nodes list
+/*  prepedGraph['nodes'] = d3.keys(
+                          d3.nest()
+                            .key(function (d){ return d.name; })
+                              .map(prepedGraph['nodes']));
+*/
+
+  console.log(prepedGraph['nodes']);
+ return prepedGraph;
 }
 
 
--- test/extensions/com.brett.sankey/sankey-data-prep-test.js
+++ test/extensions/com.brett.sankey/sankey-data-prep-test.js
@@ -2,25 +2,25 @@ import sankeyDataPrep from "../../../src/extensions/com.brett.sankeyV2/sankey-da
 
 describe("Sankey Data Preparation", function() {
   //no input test
-  it("empty dataset returns empty preped dataset", function(){
+  it.skip("empty dataset returns empty preped dataset", function(){
     let result = sankeyDataPrep([]);
      expect(result).to.deep.eq({"nodes" : [], "links" :[]});  
   })
   
   //small input test
-  it.skip("small dataset returns correct result", function(){
+  it("small dataset returns correct result", function(){
     let result = sankeyDataPrep(
       [{
       "source": "Barry",
       "target": "Elvis",
       "value" : 2
       }]);
-
+ 
     expect(result).to.equal({ 
       "nodes" : [{"name" : "Barry"}], 
-      "links" :[{"source" : 0, "target":1, "value": 2}]}
-    );
-  
+      "links" :[{"source" : 0, "target":1, "value": 2}]});
+  })
+
   it.skip("default behavior", function(){
     
   })
