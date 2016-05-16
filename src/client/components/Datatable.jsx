74e7c7a099b0392abc97a3ddccf6e4d23f177020
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-05-16 14:29:27 -0700

    add truncation to datatable

--- src/client/components/Datatable.jsx
+++ src/client/components/Datatable.jsx
@@ -2,17 +2,46 @@ import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import ReactFauxDOM from 'react-faux-dom';
 import { toRowColImmutable as toRowCol } from '../data-converter';
+import { List } from 'immutable';
 import d3 from 'd3';
 
 export const Datatable = React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
     const props = this.props;
+    const opts = this.props.tableOpts || {
+      maxRows: 3
+    }
+
+    function truncateTable(rows, limit) {
+      if (limit && limit < rows.size) {
+        return {
+          lessRows: rows.take(limit),
+          hidden: rows.size-limit
+        };
+      } else {
+        return {rows, hidden: 0};
+      }
+    }
+
+    function renderTable(rows, columns) {
+      const { lessRows, hidden } = truncateTable(rows, opts.maxRows);
+      return <div>
+        {Table(columns, lessRows, opts)}
+        <span><i>{hidden} additional row{hidden > 1 ? 's' : null} hidden</i></span>
+      </div>;
+    }
+
+
     if (props.columns && props.rows) {
-      return Table(props.columns, props.rows);
+      return renderTable(props.rows, props.columns);
     } else if (props.data) {
-      const { rows, columns } = toRowCol(props.data);
-      return Table(columns, rows);
+      if (List.isList(props.data)) {
+        const { rows, columns } = toRowCol(props.data);
+        return renderTable(rows, columns);
+      } else {
+        return <pre>{props.data}</pre>;
+      }
     } else {
       return null;
     }
@@ -20,11 +49,11 @@ export const Datatable = React.createClass({
 })
 
 
-const Table = (columns, rows) => {
-  let container = ReactFauxDOM.createElement('div');
+const Table = (columns, rows, opts) => {
 
-  var table = d3.select(container)
-    .append("table")
+
+  let el = ReactFauxDOM.createElement('table');
+  let table = d3.select(el);
 
   table.attr('class', 'table table-bordered table-hover table-condensed');
 
@@ -33,9 +62,10 @@ const Table = (columns, rows) => {
     .enter().append('td').text(d=>d)
 
   table.append('tbody')
-    .selectAll("tr").data(rows.toJS()).enter().append("tr")
+    .selectAll("tr").data(rows.toJS())
+    .enter().append("tr")
     .selectAll("td").data(d=>d).enter().append("td").text(d=>d)
 
-  return container.toReact();
+  return el.toReact();
 }
 
