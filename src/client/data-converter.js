23489c64bcc7e157e132787873fe98bc34b55952
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-05-16 19:09:14 -0700

    add a safety check to data converter

--- src/client/data-converter.js
+++ src/client/data-converter.js
@@ -1,7 +1,7 @@
 import { List } from 'immutable';
 
 export function toRowCol(data) {
-  if (data.length > 0) {
+  if (Array.isArray(data) && data.length > 0) {
     let columns = Object.keys(data[0]);
     let rows = data.map(row=> columns.map(key=>row[key]));
     return { rows, columns };
@@ -11,7 +11,7 @@ export function toRowCol(data) {
 }
 
 export function toRowColImmutable(data) {
-  if (data.size > 0) {
+  if (List.isList(data) && data.size > 0) {
     let columns = List(data.get(0).keys());
     let rows = data.map(row=> columns.map(key=>row.get(key)));
     return { rows, columns };
