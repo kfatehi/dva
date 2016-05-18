70ddc66a6ca55ef76c694e548ff5fccd968849b5
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-05-17 21:54:52 -0700

    be able to load titanic demo files with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,10 @@ module.exports = {
       { test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.md$/, loader: "raw" },
+      { test: /\.jpe?g$/, loader: "file" },
+      { test: /\.gif$/, loader: "file" },
+      { test: /\.csv$/, loader: "raw" },
     ]
   },
   plugins: [
