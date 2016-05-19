bc97a5a6c5d93975314e63ed05cb6c4302f317a0
Brett Walker <bdwalker93@yahoo.com>
2016-05-18 23:08:45 -0700

    startings of the csv to json parser... forgot to push. Probably useless now but just in case

--- src/data_parsers/index.js
+++ src/data_parsers/index.js
@@ -1,31 +1,31 @@
 export function getIds() {
-    return [
-          'csv.parser.1'
-            ]
+  return [
+    'csv.parser.1'
+    ]
 }
 
 export function getExtensions() {
-    return getIds().map(id => getSchema(id));
+  return getIds().map(id => getSchema(id));
 }
 
 export function getSchema(id) {
-    return require(`../extensions/${id}/schema.json`)
+  return require(`../extensions/${id}/schema.json`)
 }
 
 function getStyleHandle(id) {
-    try {
-          return require(`../extensions/${id}/style.useable.css`);
-            } catch (e) {
-                  return {
-                          use: () => {},
-                                unuse: () => {}
-                              }
-                    }
+  try {
+    return require(`../extensions/${id}/style.useable.css`);
+  } catch (e) {
+    return {
+      use: () => {},
+      unuse: () => {}
+    }
+  }
 }
 
 export function getModule(id) {
-    return {
-          render: require(`../extensions/${id}/index.js`).render,
-              style: getStyleHandle(id)
-                  }
+  return {
+    render: require(`../extensions/${id}/index.js`).render,
+    style: getStyleHandle(id)
+  }
 }
