09f403c8c7dd28b16a85a5d6f9f84d1cade1f379
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-05-25 12:27:02 -0700

    fix migrations

--- db/migrations/20160525005338-create-notebook.js
+++ db/migrations/20160525005338-create-notebook.js
@@ -27,6 +27,22 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
+    }).then(function() {
+      return queryInterface.bulkInsert('Notebooks', [{
+        uuid: '44a8b08b-dfd1-4a8e-aca0-7792add06e47',
+        isPublic: true,
+        name: 'Titanic Story',
+        jsonData: JSON.stringify(require('./demos/titanic')),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },{
+        uuid: 'cdc2c9a0-4fc5-452d-99b3-d57c8618c4e8',
+        isPublic: true,
+        name: 'Student Gradebook',
+        jsonData: JSON.stringify(require('./demos/students')),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }], {});
     });
   },
   down: function(queryInterface, Sequelize) {
