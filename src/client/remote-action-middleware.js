0356b6ae817eb98377ca271dce10318bbcec5575
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-05-28 14:07:53 -0700

    remove middleware debug

--- src/client/remote-action-middleware.js
+++ src/client/remote-action-middleware.js
@@ -1,5 +1,4 @@
 export default socket => store => next => action => {
-  console.log('processing action', action);
   if (action.meta && action.meta.remote) {
     socket.emit('action', Object.assign({}, action, {
       meta: {
